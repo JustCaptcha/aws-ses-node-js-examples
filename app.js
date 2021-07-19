7fabb2018eee9c12079678ed0b77ae2812709fdb
Albert Islamov <higashi166@gmail.com>
2021-07-19 01:53:51 +0000

    Add email template for customers

--- app.js
+++ app.js
@@ -1,42 +1,52 @@
 // Require objects.
 var express = require('express');
-var app     = express();
-var aws     = require('aws-sdk');
+var app = express();
+var aws = require('aws-sdk');
+var bp = require('body-parser');
+var cors = require('cors')
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+// app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Edit this with YOUR email address.
-var email   = "hello@example.com";
-    
+var email = "lovelyplanet@tutanota.com";
+
 // Load your AWS credentials and try to instantiate the object.
 aws.config.loadFromPath(__dirname + '/config.json');
 
 // Instantiate SES.
-var ses = new aws.SES();
+var ses = new aws.SES({ apiVersion: "2010-12-01" });
 
-// Verify email addresses.
 app.get('/verify', function (req, res) {
     var params = {
         EmailAddress: email
     };
-    
-    ses.verifyEmailAddress(params, function(err, data) {
-        if(err) {
+
+    ses.verifyEmailAddress(params, function (err, data) {
+        if (err) {
             res.send(err);
-        } 
+        }
         else {
             res.send(data);
-        } 
+        }
     });
 });
 
 // Listing the verified email addresses.
 app.get('/list', function (req, res) {
-    ses.listVerifiedEmailAddresses(function(err, data) {
-        if(err) {
+    ses.listVerifiedEmailAddresses(function (err, data) {
+        if (err) {
             res.send(err);
-        } 
+        }
         else {
             res.send(data);
-        } 
+        }
     });
 });
 
@@ -46,50 +56,57 @@ app.get('/delete', function (req, res) {
         EmailAddress: email
     };
 
-    ses.deleteVerifiedEmailAddress(params, function(err, data) {
-        if(err) {
+    ses.deleteVerifiedEmailAddress(params, function (err, data) {
+        if (err) {
             res.send(err);
-        } 
+        }
         else {
             res.send(data);
-        } 
+        }
     });
 });
 
-// Sending RAW email including an attachment.
-app.get('/send', function (req, res) {
-    var ses_mail = "From: 'AWS Tutorial Series' <" + email + ">\n";
-    ses_mail = ses_mail + "To: " + email + "\n";
-    ses_mail = ses_mail + "Subject: AWS SES Attachment Example\n";
-    ses_mail = ses_mail + "MIME-Version: 1.0\n";
-    ses_mail = ses_mail + "Content-Type: multipart/mixed; boundary=\"NextPart\"\n\n";
-    ses_mail = ses_mail + "--NextPart\n";
-    ses_mail = ses_mail + "Content-Type: text/html; charset=us-ascii\n\n";
-    ses_mail = ses_mail + "This is the body of the email.\n\n";
-    ses_mail = ses_mail + "--NextPart\n";
-    ses_mail = ses_mail + "Content-Type: text/plain;\n";
-    ses_mail = ses_mail + "Content-Disposition: attachment; filename=\"attachment.txt\"\n\n";
-    ses_mail = ses_mail + "AWS Tutorial Series - Really cool file attachment!" + "\n\n";
-    ses_mail = ses_mail + "--NextPart--";
-    
+app.post('/testargs', function (req, res) {
+    console.log(req.body)
+});
+
+app.post('/sendEmail', function (req, res) {
     var params = {
-        RawMessage: { Data: new Buffer(ses_mail) },
-        Destinations: [ email ],
-        Source: "'AWS Tutorial Series' <" + email + ">'"
+        Source: email,
+        Destination: {
+            BccAddresses: [ ],
+            CcAddresses: [],
+            ToAddresses: [
+                req.body.email
+            ]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8",
+                    Data: `<h1>Hello ${req.body.name},</h1> <p>We are perceived your email, I will reply within 24 hours.</p>`
+                },
+                Text: {
+                    Charset: "UTF-8",
+                    Data: `Hello ${req.body.name}, we are perceived your email. I will reply to you within 24 hours`
+                }
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: "We are received your email"
+            }
+        },
+        ReplyToAddresses: [
+        ],
     };
-    
-    ses.sendRawEmail(params, function(err, data) {
-        if(err) {
-            res.send(err);
-        } 
-        else {
-            res.send(data);
-        }           
+    ses.sendEmail(params, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);           // successful response
     });
 });
 
 // Start server.
-var server = app.listen(80, function () {
+var server = app.listen(8888, 'localhost', function () {
     var host = server.address().address;
     var port = server.address().port;
 
